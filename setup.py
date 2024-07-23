67d7988ee7f707e9883be183632f864b7aa3e7d2
⤷ßñï 🪽 <101978891+ZxLbni@users.noreply.github.com>
2024-07-23 12:04:00 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 #This is the place to add your URLs and setup timeout ..
-bot_urls = ['https://40590909-e728-46c8-8e1a-a872e8658836-00-11w9l40dzv1z7.sisko.replit.dev', ]
+bot_urls = ['https://replit.com/@abcdefgh96684/InstagramDownloaderBot#index.js', ]
 timeout = 20 
