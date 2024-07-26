ae077afeaf8b2cfe4fa9d0dc8c7a71a2e15aa112
⤷ßñï 🪽 <101978891+ZxLbni@users.noreply.github.com>
2024-07-26 23:36:08 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 #This is the place to add your URLs and setup timeout ..
-bot_urls = ['https://09dae815-203d-4ba3-992b-926f6cdac56c-00-qn01pxs7614r.pike.replit.dev:3000', ]
+bot_urls = ['https://b8752372-8ef7-4c32-abe8-b0276c10b0bb-00-2164x7ffq4bjo.sisko.replit.dev/Get', ]
 timeout = 20 
